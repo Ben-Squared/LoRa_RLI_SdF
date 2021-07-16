40cdb8760023c3225f205765250ebe8ad668ebc1
Ben-Squared <69542528+Ben-Squared@users.noreply.github.com>
2021-07-16 14:27:26 +0200

    mef files created

---
+++ app/inc/s_mef.h
@@ -0,0 +1,13 @@
+/*
+ * s_mef.h
+ *
+ *  Created on: 16 juil. 2021
+ *      Author: benoit
+ */
+
+#ifndef APP_INC_S_MEF_H_
+#define APP_INC_S_MEF_H_
+
+
+
+#endif /* APP_INC_S_MEF_H_ */
---
+++ app/src/s_mef.c
@@ -0,0 +1,8 @@
+/*
+ * s_mef.c
+ *
+ *  Created on: 16 juil. 2021
+ *      Author: benoit
+ */
+
+
