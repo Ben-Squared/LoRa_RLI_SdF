277e949684f362f35fb8a5539f2c75df948c84d6
Ben-Squared <69542528+Ben-Squared@users.noreply.github.com>
2021-07-16 14:25:43 +0200

    data format and frame struct

--- app/inc/frame_format.h
+++ app/inc/frame_format.h
@@ -8,6 +8,17 @@
 #ifndef APP_INC_FRAME_FORMAT_H_
 #define APP_INC_FRAME_FORMAT_H_
 
+typedef enum {slave_inquiry, broadcast_order, slave_request, slave_data, no_request
+
+}frame_type_enum;
+
+
+typedef struct {
+	uint8_t id_called; // id of expected receiver
+	frame_type_enum frame_type; // type of frame
+	uint16_t data;
+	// crc ?? mean of frame verif ??? To Be Determined
+} frame_field;
 
 
 #endif /* APP_INC_FRAME_FORMAT_H_ */
