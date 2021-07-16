6bec8b1c49ca96a6a8cebf5ff6e3e5c94fe6ee9d
Ben-Squared <69542528+Ben-Squared@users.noreply.github.com>
2021-07-16 15:32:57 +0200

    Update m_mef.h

--- app/inc/m_mef.h
+++ app/inc/m_mef.h
@@ -8,6 +8,29 @@
 #ifndef APP_INC_M_MEF_H_
 #define APP_INC_M_MEF_H_
 
+typedef enum {
+	State_init,
+	State_1,
+	State_2,
+	// etc...
+}StateEnum;
+
+
+/**
+  * @brief  function for initializing the system
+  * @param  None
+  * @retval None
+  */
+void M_System_Init(void);
+
+/**
+  * @brief  Finite State Machine handler
+  * @param  None
+  * @retval None
+  */
+void M_Mef(void);
 
 
 #endif /* APP_INC_M_MEF_H_ */
+
+
