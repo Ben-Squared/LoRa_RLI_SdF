313b21487c0a6b5abe0039ac0fec6e76aeb8f719
Ben-Squared <69542528+Ben-Squared@users.noreply.github.com>
2021-08-24 11:24:13 +0200

    add common frame.c and frame.h

---
+++ app/inc/frame.h
@@ -0,0 +1,44 @@
+/*
+ * m_frame_format.h
+ *
+ *  Created on: 16 juil. 2021
+ *      Author: benoit
+ */
+
+#ifndef APP_INC_FRAME_H_
+#define APP_INC_FRAME_H_
+
+#define MASTER
+//#undef MASTER
+
+#ifndef MASTER
+#define SLAVE
+#endif
+
+#include <stdint.h>
+
+
+typedef enum {
+	frameSlaveInquiry,
+	frameBroadcastOrder,
+	frameSlaveRequest,
+	frameSlaveData,
+	frameNoRequest
+}frameTypeEnum;
+
+
+typedef struct {
+	uint8_t idCalled; // id of expected receiver
+	frameTypeEnum frameType; // type of frame
+	uint16_t data;
+	// crc ?? mean of frame verif ??? To Be Determined
+} frameField;
+
+/**
+  * @brief  function for formatting frame according to frameType
+  * @param  frameType : {frameSlaveInquiry, frameBroadcastOrder}
+  * @retval None
+  */
+void Frame_Format(frameTypeEnum frameType);
+
+#endif /* APP_INC_FRAME_H_ */
